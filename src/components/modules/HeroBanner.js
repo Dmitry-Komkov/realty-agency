7f5ae63dcaf406a98076fc00296bc3d851c2fe26
Dmitriy Komkov <dmitriy@modelic.io>
2021-12-19 02:46:25 +0300

    Hero section ready

--- src/components/modules/HeroBanner.js
+++ src/components/modules/HeroBanner.js
@@ -38,7 +38,8 @@ const HeroBanner = () => {
         alt="Баннер"
         layout="fullWidth"
         css={css`
-          grid-area: 1/-1; 
+          grid-area: 1/-1;
+          box-shadow: 0px 4px 40px rgba(0, 0, 0, 0.25);
         `}
       />
       <Box>
