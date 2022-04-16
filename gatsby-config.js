becdd2e28350db1b05fc30f76b7f16a7b1ea537c
Dmitriy Komkov <dmitriy@modelic.io>
2022-04-17 00:48:26 +0400

    added options to gatsby-plugin-sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,15 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          formats: [`webp`, `auto`],
+          quality: 90,
+        },
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
